ceb355da0a3eb2badf37152eb1c09131bb2da376
Gina Sohn <ginasohn@stanford.edu>
2023-11-28 19:44:38 -0800

    update nanoGPT to apply speculative decoding

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
-python sample.py \
-    --init_from=gpt2 \
-    --start="What is the answer to life, the universe, and everything?" \
-    --num_samples=1 --max_new_tokens=100
+#!/bin/bash
+
+# For evaluating the KV cache
+#python sample.py --init_from=gpt2-large --start="humaneval.jsonl" --batch_size=32 --max_new_tokens=128 --prompt_length=128 --num_samples=3 --num_warmup=1
+
+# For evaluating speculative decoding
+#python sample.py --init_from=gpt2-large --start="humaneval.jsonl" --batch_size=1 --max_new_tokens=256 --prompt_length=192 --num_samples=3 --num_warmup=1
+
+python sample.py --init_from='resume' --batch_size=1 --max_new_tokens=256 --prompt_length=192 --num_samples=3 --num_warmup=1
